ec3298a1164bfe67648a11e2779f5460bed207e3
Kamanashis-Biswas <kamanashis.cse@gmail.com>
2024-09-18 03:56:27 +0600

    Profile Api Done

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,6 +1,33 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+// import { clsx, type ClassValue } from "clsx"
+// import { twMerge } from "tailwind-merge"
+
+// export function cn(...inputs: ClassValue[]) {
+//   return twMerge(clsx(inputs))
+// }
+
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const colors = [
+  "bg-[#712c4a57] text-[#ff006e] border-[1px] border-[#ff006faa]",
+  "bg-[#ff6d60a2] text-[#ff6d60a] border-[1px] border-[#ff6d60abb]",
+  "bg-[#0d6a0a2] text-[#0d6a0] border-[1px] border-[#0d6a0bb]",
+  "bg-[#4cc9f02a] text-[#4cc9f0] border-[1px] border-[#4cc9f0bb]",
+];
+
+// export const getColor = (color: number) => {
+//   if (color >= 0 && color < colors.length) {
+//     return colors[color];
+//   }
+//   return colors[0]; // Fallback to the first color if out of range
+// };
+export const getColor = (color) => {
+  if (color >= 0 && color < colors.length) {
+    return colors[color];
+  }
+  return colors[0]; // Fallback to the first color if out of range
+};
